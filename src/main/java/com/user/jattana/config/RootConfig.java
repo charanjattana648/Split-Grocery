3d94b481900b9937df0a685641720dde81ddcdd0
charanjattana648 <singhc5@douglascollege.ca>
2020-07-26 21:55:07 -0700

    Root configuration class added

---
+++ src/main/java/com/user/jattana/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.user.jattana.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.user.jattana"})
+public class RootConfig {
+
+}
