6428eeb8525fc27fafc86a9e9eff22b524466dba
charanjattana648 <singhc5@douglascollege.ca>
2020-07-27 21:29:06 -0700

    Item Interface added with four abstract methods addItem, ListAllItems, ListThisMonthItems, and deleteItem.

---
+++ src/main/java/com/user/jattana/dao/ItemDao.java
@@ -0,0 +1,17 @@
+package com.user.jattana.dao;
+
+import java.util.List;
+
+import com.user.jattana.model.Item;
+
+public interface ItemDao {
+	
+	boolean addItem(Item item);
+	
+	List<Item> getAllItems();
+	
+	List<Item> getThisMonthItems(int monthyear);
+	
+	boolean deleteItem(int id);
+
+}
