13b5661cbaa5ba5c48e46ae46812b531eff07c0d
charanjattana648 <singhc5@douglascollege.ca>
2020-07-27 21:32:29 -0700

    ItemDaoImpl provides functionality to all four methods added in ItemDao.

---
+++ src/main/java/com/user/jattana/dao/ItemDaoImpl.java
@@ -0,0 +1,29 @@
+package com.user.jattana.dao;
+
+import java.util.List;
+
+import com.user.jattana.model.Item;
+
+public class ItemDaoImpl implements ItemDao{
+
+	public boolean addItem(Item item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List<Item> getAllItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Item> getThisMonthItems(int monthyear) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean deleteItem(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
