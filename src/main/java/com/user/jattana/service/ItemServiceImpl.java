8b4e8b1f1ab8f655637353105acce62e61104c36
charanjattana648 <singhc5@douglascollege.ca>
2020-07-27 21:34:04 -0700

    ItemServiceImpl provides functionality to all four methods added in ItemService.

---
+++ src/main/java/com/user/jattana/service/ItemServiceImpl.java
@@ -0,0 +1,31 @@
+package com.user.jattana.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.user.jattana.dao.ItemDao;
+import com.user.jattana.model.Item;
+
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	ItemDao itemDao;
+	
+	public boolean addItem(Item item) {
+		return itemDao.addItem(item);
+	}
+
+	public List<Item> getAllItems() {
+		return itemDao.getAllItems();
+	}
+
+	public List<Item> getThisMonthItems(int monthyear) {
+		return itemDao.getThisMonthItems(monthyear);
+	}
+
+	public boolean deleteItem(int id) {
+		return itemDao.deleteItem(id);
+	}
+
+}
